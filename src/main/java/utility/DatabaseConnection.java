382f0efe3f4464e4f97d21479d7c9924b60ec5a0
MehtarRaditya <ryu.mehtar.2006@gmail.com>
2025-11-20 09:33:56 +0700

    Update DatabaseConnection.java

--- src/main/java/utility/DatabaseConnection.java
+++ src/main/java/utility/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ternax";
-    private static final String USER = "";
-    private static final String PASS = "";
+    private static final String USER = "YOUR-USERNAME";
+    private static final String PASS = "YOUR-PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
