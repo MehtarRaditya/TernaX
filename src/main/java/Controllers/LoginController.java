380fa05736cccb5cb4b0daa4948f32f600e0ac76
MehtarRaditya <ryu.mehtar.2006@gmail.com>
2025-11-21 10:22:33 +0700

    JavaFX Login Testing

---
+++ src/main/java/Controllers/LoginController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author mrifa
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private PasswordField txtPass;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void HeandleButtonLogin(ActionEvent event) {
+    }
+    
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.print.DocFlavor;
+import java.io.File;
+import java.net.URL;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+    public void start(Stage stage) throws Exception {
+        URL url = new File("src/main/java/Views/login.fxml").toURI().toURL();
+        Scene s = new Scene(FXMLLoader.load(url));
+        stage.setScene(s);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        launch(args);
     }
 }
